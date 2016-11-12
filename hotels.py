4dec0feaa463ea74a2e2cda5393a7b873101ae1c
Ribhav Gupta <rgupta97@calvisitor-10-105-159-17.calvisitor.1918.berkeley.edu>
2016-11-12 10:56:04 -0800

    has bugs

--- hotels.py
+++ hotels.py
@@ -7,32 +7,40 @@ app = Flask(__name__)
 def hello_world():
     r = requests.get("https://api.sandbox.amadeus.com/v1.2/hotels/search-circle?apikey=NEeYQKLjtZyWXlcUBor348kuPY5C3N8K&latitude=36.0857&longitude=-115.1541&radius=50&check_in=2016-11-15&check_out=2016-11-17")
     hotels_data = r.json()
-    hotels_options = hotels_data['results']
-    min_hotel =
-    for hotel in hotels_options:
+    hotels = hotels_data['results']
+    min_hotel = Hotel(hotels[0]['property_name'],hotels[0]['address'],hotels[0]['total_price'],hotels[0]['min_daily_rate'],hotels[0]['amenities'],hotels[0]['awards'],hotels[0]['images'],hotels[0]['rooms'])
+    hotel_objects = {}
+    for hotel in hotels:
+        key = hotel['property_name']
 
+        temp_hotel = Hotel(hotel['property_name'],hotel['address'],hotel['total_price'],hotel['min_daily_rate'],hotel['amenities'],hotels['awards'],hotel['images'],hotels['rooms'])
+
+        if hotel['property_name'] in hotel_objects.keys():
+            hotel_objects[value].append(temp_hotel)
+        else:
+            hotel_objects[value] = [temp_hotel]
+
+    return min_hotel.__str__()
 
 class Hotel:
-    def __init__(self,property_name,line1,city,postal_code,country,total_price_amount,total_price_currency,min_daily_rate_amount,min_daily_rate_currency,amenities_description,awards_provider,awards_rating,images,rooms_booking_code,room_currency,room_start_date,room_end_date,rooms_rates_price,descriptions):
+    def __init__(self,property_name,address,total_price,min_daily_rate,amenities,awards,images,rooms):
         self.property_name = property_name
-        self.line1 = line1
-        self.city = city
-        self.postal_code = postal_code
-        self.country = country
-        self.total_price_amount = total_price_amount
-        self.total_price_currency = total_price_currency
-        self.min_daily_rate_amount = min_daily_rate_amount
-        self.min_daily_rate_currency = min_daily_rate_currency
-        self.amenities_description = amenities_description
-        self.awards_provider = awards_provider
-        self.awards_rating = awards_rating
+        self.address = address
+        self.total_price = total_price
+        self.min_daily_rate = min_daily_rate
+        self.amenities = amenities
+        self.awards = awards
         self.images = images
-        self.room_booking_code = rooms_booking_code
-        self.room_currency = room_currency
-        self.room_start_date = room_start_date
-        self.room_end_date = room_end_date
-        self.room_rates_price = rooms_rates_price
-        self.descriptions = descriptions
+        self.rooms = rooms
+
+
+def __str__(self):
+    return 'Property Name: ' + self.property_name \
+           + '<br/>Address: ' + self.address \
+           + '<br/>Total Price: ' + self.total_price \
+           + '<br/>Amenities: ' + self.amenities \
+           + '<br/>rooms: ' + self.rooms
+
 
 if __name__ == '__main__':
   app.run()
