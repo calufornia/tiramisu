fdc43fe0741f0b0d3b6d9c7946dca3b0399e24d5
Sai Sunkam <sunk0015@umn.edu>
2016-11-12 14:36:34 -0600

    Updated tiramisu.py

--- tiramisu.py
+++ tiramisu.py
@@ -1,6 +1,32 @@
+import requests
+import sys
+from flask import Flask, request
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.fields.html5 import DateField
+from wtforms.validators import DataRequired
+from flask import render_template
+from flask_wtf.csrf import CsrfProtect
 
-from flask import Flask
+class APIRequestForm(Form):
+    SECRET_KEY= "YOLOSWAG"
+    origin = StringField('Origin',validators = [DataRequired()])
+    departure = DateField('Departure',validators=[DataRequired()],format='%Y-%m-%d')
+    arrival = DateField('Arrival',validators=[DataRequired()],format='%Y-%m-%d')
 
 
-if __name__ == '__main__':
-   app.run()
+app = Flask(__name__)
+app.secret_key="YOLOSWAG"
+
+
+@app.route('/',methods=['GET', 'POST'])
+def index():
+    form = APIRequestForm()
+    if request.method=='POST':
+        print("Origin is", form.origin)
+        print("Departure is", form.departure)
+        print("Arrival is", form.arrival)
+        #call to query
+        return str("search results")+form.departure.data.strftime('%Y-%m-%d')
+    else:
+        return render_template("index.html",form=form)
