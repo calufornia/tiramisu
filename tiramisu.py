43aed894073531289c7c486955873422a717dd92
Justin Lu <justinlu@berkeley.edu>
2016-11-12 05:29:25 -0800

    starter code

--- tiramisu.py
+++ tiramisu.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    r = requests.get("http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?origin=BOS&departure_date=2016-11-16--2016-11-26&duration=7--9&max_price=500&apikey=NEeYQKLjtZyWXlcUBor348kuPY5C3N8K")
+    return r.text
 
 
 if __name__ == '__main__':
